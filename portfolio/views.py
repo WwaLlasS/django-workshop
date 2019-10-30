90722f91fef4531bd26c2647bdeae9ffdd0c0cbf
Williams Rincon <wwallass.0310@gmail.com>
2019-10-29 23:50:22 -0600

    app example is done

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from portfolio.models import Project
+from portfolio.forms import ProjectForm
+
 
 # Create your views here.
+def home(request):
+    template = 'home.html'
+    context = {}
+    method = request.method
+    if method == 'GET':
+        projects = Project.objects.all()
+        project_form = ProjectForm()
+        context['project_form'] = project_form
+        context['projects'] = projects
+        return render(request, template, context)
+    if method == 'POST':
+        post = request.POST
+        files = request.FILES
+        print('technology>>', post.get('technology'))
+        project_form = ProjectForm(post, files)
+        if project_form.is_valid():
+            project_form.save()
+            return redirect('home')
+        else:
+            context['project_form'] = project_form
+            return render(request, template, context)
